c20591a7e23a22f7f6835244ca2e754332db6779
jerrej <20794967+jerrej@users.noreply.github.com>
2022-12-26 21:03:33 +0100

    Add gameboard array & display functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
 const gameBoard = (() => {
-    const board = [];
+    const board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+
+    const getField = (x, y) => {
+        return board[x][y];
+    };
+
+    const setField = (x, y, sign) => {
+        board[x][y] = sign;    
+    };
 
 
+    return { getField, setField, board }
 })();
 
 const displayController = (() => {
 
+
+    const displayGameboard = () => {
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                console.log(gameBoard.getField(x,y));
+            }
+        }
+    };
+
+    const displayGameboardArray = () => [
+        console.log(gameBoard.board)
+    ];
+
+    return { displayGameboard, displayGameboardArray }
 })();
+
+const Player = (sign) => {
+    let
+}
+
+displayController.displayGameboard();
+displayController.displayGameboardArray();
