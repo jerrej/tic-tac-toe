d5a93abf42605a43a0ad1d868e162c883d2058c7
jerrej <20794967+jerrej@users.noreply.github.com>
2022-12-27 03:18:37 +0100

    Game logic done

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
-const playerFactory = (name, sign) => {
-
-    return { name, sign };
-};
+const playerFactory = (name, sign) => { return { name, sign }; };
 
 const gameBoard = (() => {
 
     const board = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
+        "", "", "",
+        "", "", "",
+        "", "", ""
     ];
 
-    const getField = (x, y) => {
-        return board[x][y];
+    const getField = (index) => {
+        return board[index];
     };
 
-    const setField = (x, y, sign) => {
-        board[x][y] = sign;
+    const setField = (index, sign) => {
+        board[index] = sign;
     };
 
     const reset = () => {
-        for (let x = 0; x < 3; x++) {
-            for (let y = 0; y < 3; y++) {
-                board[x][y] = "";
-            }
+        for (let i = 0; x < board.length(); i++) {
         }
     }
 
-    return { getField, setField, reset, board }
+    return {
+        getField,
+        setField,
+        reset,
+        board
+    }
 })();
 
 const gameController = (() => {
@@ -35,14 +34,14 @@ const gameController = (() => {
     const playerB = playerFactory("Player 2", "X");
     let moveCounter = 1;
 
-    const makeMove = (x, y) => {
+    const makeMove = (fieldIndex) => {
         const currentPlayer = getCurrentPlayer();
-        gameBoard.setField(x, y, currentPlayer.sign);
-        if (checkWinner(x,y)) {
-            console.log("We won?")
-            isOver = true;
+        gameBoard.setField(fieldIndex, currentPlayer.sign);
+        if (checkWinner(fieldIndex)) {
+            console.log(currentPlayer.name + " won!")
+            gameOver = true;
             return;
-          }
+        }
         moveCounter++
     }
 
@@ -50,42 +49,55 @@ const gameController = (() => {
         return moveCounter % 2 === 1 ? playerA : playerB;
     };
 
-    const checkWinner = (x,y) => {
-        var winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
 
-        for (let x = 0; x < 3; x++) {
-            for (let y = 0; y < 3; y++) {
-                board[x][y] = "";
-            }
-        }
+    const checkWinner = (fieldIndex) => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        return winConditions.some(combination => {
+            return combination.every(index => {
+                return gameBoard.getField(index) === getCurrentPlayer().sign;
+            })
+        })
     };
 
-
-
-    return { makeMove, checkWinner }
+    return {
+        makeMove,
+        checkWinner,
+        playerA,
+        playerB
+    }
 })();
 
 const displayController = (() => {
-
-    const displayGameboard = () => {
-        for (let x = 0; x < 3; x++) {
-            for (let y = 0; y < 3; y++) {
-                console.log(gameBoard.getField(x, y));
-            }
-        }
-    };
 
     const displayGameboardArray = () => [
         console.log(gameBoard.board)
     ];
 
-    return { displayGameboard, displayGameboardArray }
+    return {
+        displayGameboardArray
+    }
 })();
 
-gameController.makeMove(0, 0);
-gameController.makeMove(1, 1);
-gameController.makeMove(0, 2);
-gameController.makeMove(1, 2);
-gameController.makeMove(0, 1);
+gameController.makeMove(0);
+gameController.makeMove(8);
+gameController.makeMove(1);
+gameController.makeMove(7);
+gameController.makeMove(3);
+gameController.makeMove(6);
+console.log("CHECKING" + gameController.checkWinner(0));
+console.log(gameBoard.getField(1))
+/*gameController.makeMove(4);
+gameController.makeMove(2);
+gameController.makeMove(5);
+gameController.makeMove(1);*/
 displayController.displayGameboardArray();
-console.log(gameController.checkWinner());
